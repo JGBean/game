380f7c1cde56a8c44eb045b5886addc16d0a6c31
JGBean <gyubin0712@naver.com>
2023-11-18 15:15:35 +0900

    적 무작위 움직임 적용

--- Enemy.py
+++ Enemy.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 class Enemy:
     def __init__(self, spawn_position):
@@ -7,3 +8,25 @@ class Enemy:
         self.position = np.array([spawn_position[0] - 25, spawn_position[1] - 25, spawn_position[0] + 25, spawn_position[1] + 25])
         self.center = np.array([(self.position[0] + self.position[2]) / 2, (self.position[1] + self.position[3]) / 2])
         self.outline = "#00FF00"
+        self.speed = 4  # Initial speed
+
+    def move(self):
+        # Calculate the new position based on the current speed
+        new_x = self.position[0] + (np.random.randint(0, 2) * 2 - 1) * self.speed
+
+        # Limit the new position to stay within the screen boundaries
+        new_x = max(0, min(new_x, 240 - 50))  # Assuming width of enemy is 50
+
+        # Move only in the upper half of the screen (adjust as needed)
+        new_y = max(0, min(self.position[1] + (np.random.randint(0, 2) * 2 - 1) * self.speed, 120 - 50))  # Assuming height of enemy is 50
+
+        # Update the position
+        self.position = np.array([new_x, new_y, new_x + 50, new_y + 50])  # Adjusted for assumed size
+
+        # Update the center
+        self.center = np.array([(self.position[0] + self.position[2]) / 2, (self.position[1] + self.position[3]) / 2])
+
+
+
+
+
